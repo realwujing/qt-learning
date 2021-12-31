e58667711556cdf97043fac7435d8db409045af4
yuanqiliang <yuanqiliang@uniontech.com>
2021-12-31 10:02:58 +0800

    first commit

---
+++ singals_slots/build/CMakeFiles/singals_slots_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "singals_slots_autogen"
+  "CMakeFiles/singals_slots_autogen.dir/AutogenOldSettings.txt"
+  "CMakeFiles/singals_slots_autogen"
+  "singals_slots_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/singals_slots_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
